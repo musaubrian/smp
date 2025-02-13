2bf2dc636ba3c17ed1ecb228fd2444c5faae3780
musaubrian <musaubrian15@gmail.com>
2025-02-13 15:02:29 +0300

    chore: use latest raylib version
    - switches to raylib 5.5 because I dont really feel like writing my own
      music stream validation logic when raylib already has it

--- shell.nix
+++ shell.nix
@@ -1,15 +1,17 @@
-{pkgs ? import <nixpkgs> {}}:
+{
+  pkgs ? import <nixpkgs> {},
+  unstable ? import (fetchTarball "https://github.com/nixos/nixpkgs/archive/nixos-unstable.tar.gz") {},
+}:
 pkgs.mkShell {
   buildInputs = with pkgs; [
     gcc
-    raylib
+    unstable.raylib
     wayland
     wayland-protocols
     libGL
     xorg.libX11
     libxkbcommon
   ];
-
   shellHook = ''
     export RAYLIB_WAYLAND_LIBRARY_PATH="${pkgs.wayland}/lib/libwayland-client.so"
   '';
